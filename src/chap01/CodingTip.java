e7a2a8a6b5c6383ab34bd3e77c9d55fb2fae88e7
RelaxingNap <99159723+RelaxingNap@users.noreply.github.com>
2022-02-24 12:16:40 +0900

    first commit

---
+++ src/chap01/CodingTip.java
@@ -0,0 +1,15 @@
+package chap01;
+
+public class CodingTip {
+	//hint : ctrl + space바
+	public static void main(String[] args) {	//main method 단축 키워드 : main + ctrl + space바
+		// 자동 코드 포맷팅 : ctrl + shift + f
+		// 콘솔 출력 단축 키워드 : sout + ctrl + space바
+		// 실행 : ctrl + f11
+		// 주석 : ctrl + /
+		// 창크기변경 : ctrl + m
+		// 새파일 : ctrl + n
+		// 저장 : ctrl + s
+		
+	}
+}
